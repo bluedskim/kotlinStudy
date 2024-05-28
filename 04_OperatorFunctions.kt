1efa9712d6c5b685ad516b98772af60af50d4aaa
Dennis <bluedskim@gmail.com>
2024-05-28 22:51:09 +0900

    new file Operator Functions

--- 04_OperatorFunctions.kt
+++ 04_OperatorFunctions.kt
@@ -1,4 +1,11 @@
 package org.kotlinlang.play
 
-class OperatorFunctions {
+fun main() {
+    operator fun Int.times(str: String) = str.repeat(this)       // 1
+    //The operator symbol for times() is * so that you can call the function using 2 * "Bye"
+    println(2 * "Bye ")                                          // 2
+
+    operator fun String.get(range: IntRange) = substring(range)  // 3
+    val str = "Always forgive your enemies; nothing annoys them so much."
+    println(str[0..14])                                          // 4
 }
